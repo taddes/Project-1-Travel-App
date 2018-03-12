73be5c8ab864d3f749b23c5dc8f38f734d7a6dbe
Connor Reed <connorreed@wifi_guest-10-38-104-95.uncc.edu>
2018-03-12 12:37:23 -0400

    firebase code and basic links

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAx4ahxYbpCVsvkg0DJepiILkxWVepORBM",
+    authDomain: "my-travels-project.firebaseapp.com",
+    databaseURL: "https://my-travels-project.firebaseio.com",
+    projectId: "my-travels-project",
+    storageBucket: "",
+    messagingSenderId: "203317717944"
+  };
+  firebase.initializeApp(config);
